1719ddaa3d07ca8a6db0b33b9fa1c36aa632cd3b
sven-eliasen <jo.pettersen@pm.me>
2022-01-17 13:32:57 +0100

    print input

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,45 @@
 
 use core::fmt::Write;
 use uefi::prelude::*;
-use uefi::{Event, ResultExt};
-use uefi::proto::console::text::{Color, Input, Output};
+use uefi::{Char16, ResultExt};
+use uefi::proto::console::text::{Color, Key, Output};
 
-
-fn write_to_screen(stdout: &mut Output) {
-    stdout.set_color(Color::Red, Color::Black);
-    stdout.write_str(include_str!("text.txt"));
-    stdout.write_str("\n> ");
+fn init_screen(stdout: &mut Output) {
+    stdout.clear().unwrap().unwrap();
+    stdout.set_color(Color::Red, Color::Black).unwrap().unwrap();
+    stdout.write_str(include_str!("text.txt")).unwrap();
+    stdout.write_str("\n> ").unwrap();
 }
 
-unsafe fn get_key_event(stdin: &mut Input) -> Event {
-    stdin.wait_for_key_event().unsafe_clone()
+fn write_char(stdout: &mut Output, char_key: Char16) {
+    stdout.write_char(char::from(char_key)).unwrap();
 }
 
 #[entry]
 unsafe fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap_success();
 
-    write_to_screen(system_table.stdout());
-    let mut events = [get_key_event(system_table.stdin())];
+    let mut st = system_table.unsafe_clone();
+    let stdin = st.stdin();
+    //let mut events = [stdin.wait_for_key_event().unsafe_clone()];
 
-    let boot_services = system_table.boot_services();
+    init_screen(system_table.stdout());
 
+    //let boot_services = st.boot_services();
     loop {
-        boot_services.wait_for_event(&mut events);
+        //boot_services.wait_for_event(&mut events).unwrap();
+        let key = stdin.read_key().unwrap().unwrap();
+        match key {
+            None => {}
+            Some(key_c) => {
+                match key_c {
+                    Key::Printable(char_key) => {
+                        write_char(system_table.stdout(), char_key)
+                    }
+                    Key::Special(_) => {}
+                }
+            }
+        }
     }
 
     //Status::SUCCESS
